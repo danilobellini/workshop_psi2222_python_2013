89ecdabf39534055fe8cfc71c0ea89cd61c0345e
Dojo <danilo.bellini@gmail.com>
2013-10-18 13:06:54 -0300

    Script que gerou os commits iniciais

---
+++ primeiros_commits.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+git init
+git config user.name Dojo
+git config user.email @
+git add 20131009/test_fizzbuzz.py
+git commit -m "Código final dojo primeiro dia (FizzBuzz)"
+
+git config user.name "Danilo de Jesus da Silva Bellini"
+git config user.email danilo.bellini@gmail.com
+git add 20131009/flask_try.py
+git commit -m "Discussão primeiro dia: FizzBuzz em Flask"
+
+git config user.name Dojo
+git config user.email @
+git add 20131010/*.py
+git commit -m "Código final dojo segundo dia (OCR números ASCII)"
+
+git config user.name Danilo de Jesus da Silva Bellini
+git config user.email danilo.bellini@gmail.com
+git add primeiros_commits.sh
+git commit -m "Script que gerou os commits iniciais"
+
